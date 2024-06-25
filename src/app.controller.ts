15d94c7d1d513d4c2697c4432502269c1424bfd4
david354 <david.kouame@data354.co>
2024-06-25 11:03:55 +0000

    update code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags("hello")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Say Hello world an item' })
+ // @ApiBody({ type: CreateItemDto })
+  @ApiResponse({ status: 200, description: 'The item has been created.' })
   getHello(): string {
     return this.appService.getHello();
   }
